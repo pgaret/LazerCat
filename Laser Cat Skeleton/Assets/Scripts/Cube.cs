70c4661a7143531c8816881b4ddc654481284acc
Peregrin <pgaret@att.net>
2015-09-22 18:02:30 -0400

    Things

--- Laser Cat Skeleton/Assets/Scripts/Cube.cs
+++ Laser Cat Skeleton/Assets/Scripts/Cube.cs
@@ -3,31 +3,37 @@ using System.Collections;
 
 public class Cube : MonoBehaviour {
 
-	public float rotateSpeed = 0;
+	public float setrotateSpeed;
+	float rotateSpeed;
+
+	Vector3 axisofRotation;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+		axisofRotation = transform.GetChild (2).transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		transform.Rotate(Vector3.right*Time.deltaTime*rotateSpeed);
+		transform.RotateAround(axisofRotation, Vector3.up, rotateSpeed);
 
 		GameObject[] spheres = GameObject.FindGameObjectsWithTag ("Sphere");
 		for (int i = 0; i < spheres.Length; i++)
 		{
 			if (spheres[i].GetComponent<SphereCollider>().bounds.Intersects(transform.GetChild(0).GetComponent<BoxCollider>().bounds))
 			{
-				rotateSpeed = 5f;
+				rotateSpeed = setrotateSpeed;
 				Destroy (spheres[i].gameObject);
 			}
 			else if (spheres[i].GetComponent<SphereCollider>().bounds.Intersects(transform.GetChild(1).GetComponent<BoxCollider>().bounds))
 			{
-				rotateSpeed = -5f;
+				rotateSpeed = -setrotateSpeed;
 				Destroy (spheres[i].gameObject);
 			}
 		}
+
+		Debug.Log (rotateSpeed);
 	}
 }
